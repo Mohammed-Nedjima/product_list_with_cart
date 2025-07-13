efb582ab1cf106b6c0d9723258050c8bec8d1133
Mohammed Ayoub Nedjima <85318131+Mohammed-Nedjima@users.noreply.github.com>
2025-07-13 20:53:59 +0100

    tweak to pass linter 3

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,4 @@
 import { useState } from "react";
-import { CartContext } from "./components/cartContexProvider";
 import ProductGallery from "./components/productGallery/productGallery";
 import Cart from "./components/cart/cart";
 import Modal from "./components/modal/modal";
